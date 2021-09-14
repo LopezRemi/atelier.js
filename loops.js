633211e698bc5d33b9d625fd0351ed46a0a092da
Ryuu76000 <r.lopez76000@gmail.com>
2021-09-14 16:54:56 +0200

    first commit

---
+++ loops.js
@@ -0,0 +1,18 @@
+console.log(" ##### exemple de boucles ##### \n")
+
+//While loop exemple
+var i = 0;
+
+var students = [
+    "Theo",
+    "Louis",
+    "Cécile",
+    "Chloé",
+    "brandon",
+    "timoté"
+];
+
+while (i < students.length) {
+    console.log(students[i]);
+    i++;  
+}
