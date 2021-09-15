bfdd3985b0d65393f091572181671a1ff6927cd2
Ryuu76000 <r.lopez76000@gmail.com>
2021-09-15 15:13:14 +0200

    ajout loop

--- loops.js
+++ loops.js
@@ -1,4 +1,4 @@
-console.log(" ##### exemple de boucles ##### \n")
+console.log(" ##### exemple of loops ##### \n")
 
 //While loop exemple
 var i = 0;
@@ -12,11 +12,42 @@ var students = [
     "timoté"
 ];
 
-while (i < students.length) {
-    console.log(students[i]);
-    i++;  
+// while (i < students.length) {
+//     console.log(students[i]);
+//     i++;  
+// }
+
+//for loop exemple
+
+// for (var j = 0; j < students.length; j++) {
+//     console.log(students[j]);
+// }
+
+//for of / for in loops exemple
+
+for(var student of students) {
+    console.log(student);
 }
 
-for (var j = 0; j < students.length; j++) {
-    console.log(students[j]);
+for(var index in students){
+    console.log(students[index]);
+}
+
+for(var index in students){
+console.log("Etudiant numéro : " + (parseInt(index)+1) + " " + students[index]);
+}
+
+for(var index in students){
+    console.log(index);
+}
+    
+var human = {
+    "name" : "henry",
+    "age" : 45,
+    "sexe" : "male",
+    "country" : "france"
+}
+
+for(var key in human) {
+    console.log(key + " : " + human[key]);
 }
